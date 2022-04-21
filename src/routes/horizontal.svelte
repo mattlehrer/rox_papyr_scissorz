32cbf454a9c4a62563a93e8b1fb05401f4c519ae
Matt Lehrer <lehrerm@gmail.com>
2022-04-21 15:54:33 +0200

    setup iframes

---
+++ src/routes/horizontal.svelte
@@ -0,0 +1,11 @@
+<div class="min-h-full flex">
+	<iframe
+		id="inlineFrameExample"
+		title="Inline Frame Example"
+		width="40%"
+		height="1000"
+		src="https://creator.voiceflow.com/prototype/625403b7b0ed566548628297"
+		allow="microphone"
+	/>
+	<iframe id="inlineFrameExample" title="Inline Frame Example" width="60%" height="1000" src="/" />
+</div>
---
+++ src/routes/vertical.svelte
@@ -0,0 +1,11 @@
+<div class="min-h-full">
+	<iframe id="inlineFrameExample" title="Inline Frame Example" width="100%" height="600" src="/" />
+	<iframe
+		id="inlineFrameExample"
+		title="Inline Frame Example"
+		width="100%"
+		height="800"
+		src="https://creator.voiceflow.com/prototype/625403b7b0ed566548628297"
+		allow="microphone"
+	/>
+</div>
